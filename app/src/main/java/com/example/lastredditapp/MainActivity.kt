0fb4c87b1e633f204e5042ccc49dc440337d4254
Stepanov Egor <stepegorr@gmail.com>
2024-05-26 14:13:20 +0300

    Open image in big format

--- app/src/main/java/com/example/lastredditapp/MainActivity.kt
+++ app/src/main/java/com/example/lastredditapp/MainActivity.kt
@@ -1,41 +1,75 @@
 package com.example.lastredditapp
 
-import android.content.Context
-import android.content.SharedPreferences
+import android.app.AlertDialog
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlin.math.min
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PostsAdapter.OnImageClickListener {
+    //змінна поточної сторінки користувача
     private var currentPage = 0
+
+    //змінна максимальної кількості постів на одній сторінки
     private val pageSize = 5
+
+    //змінна поточної відкритої картинки користувача
+    private var openImageNow:String = ""
+
+    //Створення діалогу для відображення картинок у збільшеному форматі
+    fun createSimpleDialog(image: ImageView) {
+        val builder = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val dialogLayout = inflater.inflate(R.layout.dialog_image, null)
+        var imageView = dialogLayout.findViewById<ImageView>(R.id.dialogImage)
+
+        imageView.setImageDrawable(image.drawable)
+        builder.setView(dialogLayout)
+
+        builder.setPositiveButton("OK") { dialog, which ->
+            openImageNow=""
+        }
+        builder.setNeutralButton("Cancel") { dialog, which ->
+           openImageNow=""
+        }
+        builder.show()
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //збереження змінної поточної сторінки користувача з попереднього закриття
         if (savedInstanceState != null) {
             currentPage = savedInstanceState.getInt("currentPage", 0)
+            openImageNow = savedInstanceState.getString("openImageNow", "")
         }
 
+        // ініціалізація масиву з постами
         val postsList: RecyclerView = findViewById(R.id.posts_list)
         val posts = arrayListOf<Posts>()
+
+        //додавання кнопок Вперед та Назад
         val btnNextPage: Button = findViewById(R.id.button_forward_main)
         val btnPreviousPage: Button = findViewById(R.id.button_back_main)
 
+        //функція яка контролює відображення постів
         fun loadPosts() {
             val startIndex = currentPage * pageSize
             val endIndex = min(startIndex + pageSize, posts.size)
             val subList = posts.subList(startIndex, endIndex)
             postsList.layoutManager = LinearLayoutManager(this)
-            postsList.adapter = PostsAdapter(subList, this)
+            postsList.adapter = PostsAdapter(subList, this, this)
         }
 
+        //додавання постів в масив
         posts.add(Posts(1,"affre","foto_1","Title post 1","Link_post_1","Many text for post 1", "1234", "32454"))
         posts.add(Posts(2,"fdssww","foto_2","Title post 2","Link_post_2","Many text for post 2", "354", "12454"))
         posts.add(Posts(3,"fadfdd","foto_3","Title post 3","Link_post_3","Many text for post 3", "654", "65454"))
@@ -46,23 +80,55 @@ class MainActivity : AppCompatActivity() {
         posts.add(Posts(8,"asdfsde","foto_8","Title post 8","Link_post_8","Many text for post 8", "12", "24254"))
         posts.add(Posts(9,"asfgfgre","foto_9","Title post 9","Link_post_9","Many text for post 9", "14", "32454"))
         posts.add(Posts(10,"asfgsfde","foto_10","Title post 10","Link_post_10","Many text for post 10", "784", "15654"))
+
+        //виклик функції про відображення стану додатку до закриття
         loadPosts()
+        if(openImageNow != ""){
+            val imageView = ImageView(this)
+            val resourceId = resources.getIdentifier(openImageNow, "drawable", packageName)
+            imageView.setImageResource(resourceId)
+            createSimpleDialog(imageView)
+
+        }
+
+        //перехід на попередню сторінку
         btnPreviousPage.setOnClickListener {
             if (currentPage > 0) {
                 currentPage--
                 loadPosts()
             }
         }
+        //перехід на наступну сторінку
         btnNextPage.setOnClickListener {
             if ((currentPage + 1) * pageSize < posts.size) {
                 currentPage++
                 loadPosts()
             }
         }
+
+
+
+
     }
 
+    //збереження змінної про поточну сторінку користувача
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putInt("currentPage", currentPage)
+        outState.putString("openImageNow", openImageNow)
+    }
+
+    //перевизначення функції про клік на картинку для її відображення у діалогу
+    override fun onImageClick(posts: Posts) {
+        //збереження картинки якщо додаток зактиють
+        openImageNow = posts.image
+
+        //конвертація картинки у тип ImageView
+        val imageView = ImageView(this)
+        val resourceId = resources.getIdentifier(posts.image, "drawable", packageName)
+        imageView.setImageResource(resourceId)
+
+        //відправляння її в функцію діалогу
+        createSimpleDialog(imageView)
     }
 }
--- app/src/main/java/com/example/lastredditapp/PostsAdapter.kt
+++ app/src/main/java/com/example/lastredditapp/PostsAdapter.kt
@@ -6,10 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class PostsAdapter(var posts: List<Posts>, var context: Context) :RecyclerView.Adapter<PostsAdapter.MyViewHolder>(){
+class PostsAdapter(var posts: List<Posts>, var context: Context,  var clickListener: OnImageClickListener) :RecyclerView.Adapter<PostsAdapter.MyViewHolder>(){
 
+    //інтерфейс для створення івенту натискання на картинку
+    interface OnImageClickListener {
+        fun onImageClick(posts: Posts)
+    }
     class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
         val author: TextView = view.findViewById(R.id.author_posts)
         val timepass: TextView = view.findViewById(R.id.time_pas_posts)
@@ -35,21 +40,27 @@ class PostsAdapter(var posts: List<Posts>, var context: Context) :RecyclerView.A
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.author.text = posts[position].author
 
+        //ми отримуємо данні про час у секундах в форматі строки, а нам потрібно їх відобразити у ворматі X hours ago
         var timePass = posts[position].time_pas.toInt()
         timePass = timePass /60 /60
+        holder.timepass.text = "$timePass hours ago"
 
-        holder.timepass.text = "$timePass hr.ago"
         holder.title.text = posts[position].title
         holder.urltext.text = posts[position].link
         holder.comment.text = posts[position].count_comm
 
+        //знаходження картинки для цього поста
         val imageId = context.resources.getIdentifier(
             posts[position].image,
             "drawable",
             context.packageName
         )
-
         holder.image.setImageResource(imageId)
+
+        //виклик функції при натисканні на картинку
+        holder.image.setOnClickListener{
+            clickListener.onImageClick(posts[position])
+        }
 
     }
 }
